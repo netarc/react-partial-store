2a2c15ab7335e2e94cf47bf5f423e5d203dec40a
Josh Hollenbeck <josh.hollenbeck@citrusbyte.com>
2016-02-19 01:09:20 -0800

    Expand onlyActions to accept a string array

--- test/createDataset.spec.js
+++ test/createDataset.spec.js
@@ -1,58 +1,60 @@
 var chai = require('chai')
   , RPS = require('../lib/index')
-  , assert = chai.assert;
+  , expect = chai.expect;
 
 
 describe("createDataset", function() {
   describe("when invoked", function() {
     describe("with no arguments", function() {
       it("should create a dataset", function() {
-        var dataset = RPS.createDataset();
-        assert(dataset instanceof RPS.createDataset.prototype, "dataset was not instance of Dataset");
+        var result = RPS.createDataset();
+        expect(result).to.be.an.instanceof(RPS.createDataset.prototype);
       });
     });
 
     describe("with an invalid argument", function() {
       it("should throw an error", function() {
-        assert.throws(function(){
+        expect(function() {
           RPS.createDataset(123);
-        });
+        }).to.throw(Error);
       });
     });
 
     describe("with a definition argument", function() {
       it("should properly create a dataset", function() {
-        var dataset = RPS.createDataset({uri: "/projects"});
-        assert(dataset instanceof RPS.createDataset.prototype, "dataset was not instance of Dataset");
-        assert.deepEqual(dataset.definition, {uri: "/projects"});
+        var result = RPS.createDataset({uri: "/projects"});
+        expect(result).to.be.an.instanceof(RPS.createDataset.prototype);
+        expect(result.definition).to.deep.equal({uri: "/projects"});
       });
 
       it("should throw an error with an invalid option", function() {
-        assert.throws(function(){
+        expect(function() {
           RPS.createDataset({barfoo: 123});
-        });
+        }).to.throw(Error);
       });
 
       it("should throw an error on invalid option values", function() {
-        assert.throws(function(){
+        expect(function() {
           RPS.createDataset({partial: 123});
-        });
+        }).to.throw(Error);
 
-        assert.throws(function(){
+        expect(function() {
           RPS.createDataset({fragments: 123});
-        });
+        }).to.throw(Error);
 
-        assert.throws(function(){
+        expect(function() {
           RPS.createDataset({uri: 123});
-        });
+        }).to.throw(Error);
 
-        assert.throws(function(){
+        expect(function() {
           RPS.createDataset({actions: []});
-        });
+        }).to.throw(Error);
+      });
 
-        assert.throws(function(){
+      it("should not throw an error on valid option values", function() {
+        expect(function() {
           RPS.createDataset({onlyActions: []});
-        });
+        }).to.not.throw(Error);
       });
     });
   });
--- test/createStore.spec.js
+++ test/createStore.spec.js
@@ -2,7 +2,7 @@ var chai = require('chai')
   , RPS = require('../lib/index')
   , StoreSet = require("../lib/StoreSet")
   , testHelper = require('./testHelper')
-  , assert = chai.assert;
+  , expect = chai.expect;
 
 
 describe("createStore", function() {
@@ -11,36 +11,33 @@ describe("createStore", function() {
 
     describe("with no arguments", function() {
       it("should create an anonymous store", function() {
-        assert.sameMembers(Object.keys(StoreSet), []);
-        var store = RPS.createStore();
-        assert(store instanceof RPS.createStore.prototype, "store was not instance of Store");
-        assert.sameMembers(Object.keys(StoreSet), [store.definition.type]);
+        var result = RPS.createStore();
+        expect(result).to.be.an.instanceof(RPS.createStore.prototype);
+        expect(Object.keys(StoreSet)).to.have.members([result.definition.type]);
       });
     });
 
     describe("with an invalid argument", function() {
       it("should throw an error", function() {
-        assert.throws(function(){
+        expect(function() {
           RPS.createStore(123);
-        });
+        }).to.throw(Error);
       });
     });
 
     describe("with a string argument", function() {
       it("should create a named store", function() {
-        assert.sameMembers(Object.keys(StoreSet), []);
-        var store = RPS.createStore("foobar");
-        assert(store instanceof RPS.createStore.prototype, "store was not instance of Store");
-        assert.sameMembers(Object.keys(StoreSet), ["foobar"]);
+        var result = RPS.createStore("foobar");
+        expect(result).to.be.an.instanceof(RPS.createStore.prototype);
+        expect(Object.keys(StoreSet)).to.have.members(["foobar"]);
       });
 
       it("should throw an error a named store already exists", function() {
-        assert.sameMembers(Object.keys(StoreSet), []);
-        var store_1 = RPS.createStore("foobar");
-        assert(store_1 instanceof RPS.createStore.prototype, "store was not instance of Store");
-        assert.sameMembers(Object.keys(StoreSet), ["foobar"]);
+        var result = RPS.createStore("foobar");
+        expect(result).to.be.an.instanceof(RPS.createStore.prototype);
+        expect(Object.keys(StoreSet)).to.have.members(["foobar"]);
 
-        assert.throws(function(){
+        expect(function() {
           RPS.createStore("foobar");
         }, TypeError, "found existing Store of");
       });
@@ -48,49 +45,49 @@ describe("createStore", function() {
 
     describe("with a string and dataset definition argument", function() {
       it("should properly create the store and return a dataset", function() {
-        assert.sameMembers(Object.keys(StoreSet), []);
         var result = RPS.createStore("foobar", {uri: "/projects"});
-        assert(result instanceof RPS.createDataset.prototype, "result was not instance of Dataset");
-        assert.sameMembers(Object.keys(StoreSet), ["foobar"]);
-        assert.deepEqual(result.definition, {uri: "/projects"});
+        expect(result).to.be.an.instanceof(RPS.createDataset.prototype);
+        expect(Object.keys(StoreSet)).to.have.members(["foobar"]);
+        expect(result.definition).to.deep.equal({uri: "/projects"});
       });
     });
 
     describe("with a definition argument", function() {
       it("should properly create a named store", function() {
-        assert.sameMembers(Object.keys(StoreSet), []);
         var result = RPS.createStore({type: "foobar"}, {uri: "/projects"});
-        assert(result instanceof RPS.createDataset.prototype, "result was not instance of Dataset");
-        assert.sameMembers(Object.keys(StoreSet), ["foobar"]);
-        assert.deepEqual(result.definition, {uri: "/projects"});
+        expect(result).to.be.an.instanceof(RPS.createDataset.prototype);
+        expect(Object.keys(StoreSet)).to.have.members(["foobar"]);
+        expect(result.definition).to.deep.equal({uri: "/projects"});
       });
 
       it("should throw an error with an invalid option", function() {
-        assert.throws(function(){
+        expect(function() {
           RPS.createStore({type: "foobar", barfoo: 123});
-        });
+        }).to.throw(Error);
       });
 
       it("should throw an error on invalid option values", function() {
-        assert.throws(function(){
+        expect(function() {
           RPS.createStore({type: 123});
-        });
+        }).to.throw(Error);
 
-        assert.throws(function(){
+        expect(function() {
           RPS.createStore({initialParams: 123});
-        });
+        }).to.throw(Error);
 
-        assert.throws(function(){
+        expect(function() {
           RPS.createStore({uri: 123});
-        });
+        }).to.throw(Error);
 
-        assert.throws(function(){
+        expect(function() {
           RPS.createStore({actions: []});
-        });
+        }).to.throw(Error);
+      });
 
-        assert.throws(function(){
+      it("should not throw an error on valid option values", function() {
+        expect(function() {
           RPS.createStore({onlyActions: []});
-        });
+        }).to.not.throw(Error);
       });
     });
   });
